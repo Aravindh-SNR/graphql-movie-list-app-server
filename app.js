f2ad965eb6da2e4f3c91af66e3d55703e168d723
Aravindh-SNR <aravindhsnr@gmail.com>
2020-03-10 17:13:35 +0530

    created graphql schema and connected server with mongo db

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+
+// Get MongoDB URI from the config folder
+const db = config.get('mongoURI');
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(console.log);
+
+// Use middleware returned by the graphqlHTTP function to respond to requests
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Listening for requests on port 4000');
+});
